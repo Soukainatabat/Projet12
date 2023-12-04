8ee46b86573b8864c59b2ace91c33e3ed77cf2cd
Soukainatabat <stabat292001@gmail.com>
2023-12-04 10:39:13 +0100

    version 1

---
+++ src/test/java/com/example/kafkatp1/KafkaTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkatp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
